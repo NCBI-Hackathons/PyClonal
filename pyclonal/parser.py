f8a07be31934f205b23de15cb26a07cd0a9c8b28
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2018-08-07 11:55:15 -0400

    Added custom format spec

--- pyclonal/parser.py
+++ pyclonal/parser.py
@@ -4,17 +4,38 @@ import sys
 
 import pandas as pd
 
+FORMATS = {
+        'mixcr': ('cloneCount', 'aaSeqCDR3', None),
+        'changeo': ('DUPCOUNT', 'CLONE_CDR3_AA', 'SAMPLE'),
+        'vdjtools': ('count', 'cdr3aa', None),
+        'mitcr': ('Read_count', 'CDR3_amino_acid_sequence', None),
+        'immunoseq': ('count (templates/reads)', 'aminoAcid', None)
+        }
+
+
+FMT_COLS = {
+        "mixcr": ["clonalSequenceQuality", "minQualFR1", "allDAlignments"],
+        "changeo": ["SEQUENCE_ID", "JUNCTION_LENGTH", "CLONE_CDR3_AA"],
+        "vdjtools": ["freq", "cdr3nt", "cdr3aa"],
+        "immunoseq": ["aminoAcid", "frequencyCount", "cdr3Length"],
+        "mitcr": ["Read count", "CDR3 amino acid sequence", "V segments"],
+        }
+
+
 class BaseParser:
+    def __init__(self, filename, fmt=None, fmt_cols=None):
+        '''
+        Sample format parser.
+        Users can provide their custom formats.
+        '''
 
-    FIELDSET = {
-            'mixcr': ('cloneCount', 'aaSeqCDR3', None),
-            'changeo': ('DUPCOUNT', 'CLONE_CDR3_AA', 'SAMPLE'),
-            'vdjtools': ('count', 'cdr3aa', None),
-            'mitcr': ('Read_count', 'CDR3_amino_acid_sequence', None),
-            'immunoseq': ('count (templates/reads)', 'aminoAcid', None)
-            }
+        self.FORMATS = FORMATS
+        if fmt is not None:
+            self.FORMATS.update(fmt)
+        self.FMT_COLS = FMT_COLS
+        if fmt_cols is not None:
+            self.FMT_COLS.update(fmt_cols)
 
-    def __init__(self, filename):
         fmt = self._detect_format(filename)
         self._filename = filename
         self.cnt_field, self.cdr3_field, self.sample = self._get_field_names(fmt)
@@ -27,24 +48,15 @@ class BaseParser:
         with open(filename, 'rt') as fh:
             first_line = next(fh)
 
-        # Look for an arbitrary subset of column names to detect format:
-        detect_columns = {
-                "mixcr": ["clonalSequenceQuality", "minQualFR1", "allDAlignments"],
-                "changeo": ["SEQUENCE_ID", "JUNCTION_LENGTH", "CLONE_CDR3_AA"],
-                "vdjtools": ["freq", "cdr3nt", "cdr3aa"],
-                "immunoseq": ["aminoAcid", "frequencyCount", "cdr3Length"],
-                "mitcr": ["Read count", "CDR3 amino acid sequence", "V segments"],
-        }
-
-        for type, column_names in detect_columns.items():
+        for fmt, column_names in self.FMT_COLS.items():
             if all([column_name in first_line for column_name in column_names]):
-                print("%s looks like a %s file" % (filename, type))
-                return type
+                print("%s looks like a %s file" % (filename, fmt))
+                return fmt
 
         raise Exception("Unable to detect format of %s" % filename)
 
     def _get_field_names(self, fmt):
-        return self.FIELDSET[fmt]
+        return self.FORMATS[fmt]
 
     def getSample(self, row):
         if self.sample is None:
