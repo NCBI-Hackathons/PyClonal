8d11e685130a521514a2d7bd7403056d42af5345
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2018-08-08 10:02:59 -0400

    pyclonal.io refactor

--- pyclonal/io.py
+++ pyclonal/io.py
@@ -1,71 +1,86 @@
 import pandas as pd
 from .parser import BaseParser
 
-
-def combineFiles(filenames, fmt=None, fmt_cols=None):
-    """
-    Given a list of filenames, read and parse all of them.
-
-    Returns: two dataframes, a samples dataframe and a sequences dataframe.
-
-    The samples dataframe has one row per sample, and one column per unique clone.
-    The sequences dataframe has one row per clone, mapping clone index to clone sequence.
-    """
-
-    sequence_indices = {}
-    samples = {}
-
-    for filename in filenames:
-        parseFile(filename, sequence_indices, samples, fmt, fmt_cols)
-
-    samples = sorted(samples.items())
-    samples_df = pd.DataFrame([p[1] for p in samples])
-    samples_df = samples_df.set_index("Sample").fillna(0)
-
-    sequences_df = pd.DataFrame(
-            sorted([(v, k) for (k, v) in sequence_indices.items()]),
-            columns=["Index", "Sequence"])
-    sequences_df = sequences_df.set_index("Index")
-
-    return samples_df, sequences_df
+class FmtReader:
+    '''
+    Handles reading the data from a list of files (of possibly different
+    formats. Format specification can be either provided as a list of 
+    columns to extract data from (`fmt`)  along with the columns that
+    define the format (`fmt_cols`) or detected from the file.
+    '''
+    def __init__(self, filenames, fmt=None, fmt_cols=None):
+        self.filenames = filenames
+        self.fmt = fmt
+        self.fmt_cols = fmt_cols
 
 
-def parseFile(filename, sequence_indices=None, samples=None, fmt=None,
-        fmt_cols=None):
-    """
-    Parses a file, returning a sequence_id => count mapping.
+    def process_files(self):
+        """
+        Given a list of filenames, read and parse all of them.
 
-    Accumulates the results in `sequence_indices` and `samples` for use
-    in combining multiple files: the same sequence in multiple files will
-    use the same index.
+        Returns: two dataframes, a samples dataframe and a sequences dataframe.
 
-    `format` can be passed explicitly, otherwise auto-detection is attempted.
-    """
+        The samples dataframe has one row per sample, and one column per unique clone.
+        The sequences dataframe has one row per clone, mapping clone index to clone sequence.
+        """
 
-    parser = BaseParser(filename, fmt, fmt_cols)
-
-    if sequence_indices is None:
         sequence_indices = {}
         samples = {}
 
+        for filename in self.filenames:
+            self.parse_file(filename, sequence_indices, samples)
 
-    df = pd.read_table(filename)
+        samples = sorted(samples.items())
+        samples_df = pd.DataFrame([p[1] for p in samples])
+        samples_df = samples_df.set_index("Sample").fillna(0)
 
-    # Map column names to ones that can be accessed from Python:
-    # df.columns = [c.replace(' ', '_') for c in df.columns]
+        sequences_df = pd.DataFrame(
+                sorted([(v, k) for (k, v) in sequence_indices.items()]),
+                columns=["Index", "Sequence"])
+        sequences_df = sequences_df.set_index("Index")
 
-    for _,row in df.iterrows():
-        sample_name = parser.getSample(row)
-        if sample_name not in samples:
-            samples[sample_name] = {"Sample": sample_name}
-        sample = samples[sample_name]
+        return samples_df, sequences_df
 
-        sequence = parser.getSequence(row)
-        if sequence not in sequence_indices:
-            sequence_indices[sequence] = len(sequence_indices)
-        sequence_idx = sequence_indices[sequence]
 
-        count = parser.getCount(row)
-        sample[sequence_idx] = sample.get(sequence_idx, 0) + count
+    def parse_file(self, filename, sequence_indices=None, samples=None):
+        """
+        Parses a file, returning a sequence_id => count mapping.
 
-    return sequence_indices, samples
+        Accumulates the results in `sequence_indices` and `samples` for use
+        in combining multiple files: the same sequence in multiple files will
+        use the same index.
+
+        `format` can be passed explicitly, otherwise auto-detection is attempted.
+        """
+
+        parser = BaseParser(filename, self.fmt, self.fmt_cols)
+
+        if sequence_indices is None:
+            sequence_indices = {}
+            samples = {}
+
+
+        df = pd.read_table(filename)
+
+        # Map column names to ones that can be accessed from Python:
+        # df.columns = [c.replace(' ', '_') for c in df.columns]
+
+        for _,row in df.iterrows():
+            sample_name = parser.getSample(row)
+            if sample_name not in samples:
+                samples[sample_name] = {"Sample": sample_name}
+            sample = samples[sample_name]
+
+            sequence = parser.getSequence(row)
+            if sequence not in sequence_indices:
+                sequence_indices[sequence] = len(sequence_indices)
+            sequence_idx = sequence_indices[sequence]
+
+            count = parser.getCount(row)
+            sample[sequence_idx] = sample.get(sequence_idx, 0) + count
+
+        return sequence_indices, samples
+
+def combineFiles(filenames, fmt=None, fmt_cols=None):
+    reader = FmtReader(filenames, fmt, fmt_cols)
+    return reader.process_files()
